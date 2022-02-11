091ba72da541011a9481ac859cbdf9958f388056
kimjinsoo <1004kjs_@naver.com>
2022-02-11 14:01:45 +0900

    api/index.js 변경

--- api/index.js
+++ api/index.js
@@ -1,19 +1,14 @@
 import express from "express";
-<<<<<<< HEAD
 import auth from "./controllers/auth.js";
 import univerity from "./controllers/university.js";
 import interest from "./controllers/interest.js";
+import profile from "./controllers/profile.js";
 
 const router = express.Router();
 
 router.use("/auth", auth);
 router.use("/university", univerity);
 router.use("/interest", interest);
-=======
-import interest from "../services/interest/get_interest.js";
-const router = express.Router();
-
-router.get("/", interest);
->>>>>>> develop
+router.use("/profile");
 
 export default router;
