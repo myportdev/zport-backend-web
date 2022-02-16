1705ddfb29585eded46af2751a03087cf20cc5f6
kimjinsoo <1004kjs_@naver.com>
2022-02-16 22:17:58 +0900

    User model 수정

--- models/User.js
+++ models/User.js
@@ -57,6 +57,11 @@ const userSchema = Schema({
             ref: "Interest",
         },
     ],
+
+    promotion: {
+        type: Boolean,
+        default: false,
+    },
 });
 
 export default mongoose.model("User", userSchema);
