1b2dc3997be9fec40c6028ef0671c05c112061e1
kimjinsoo <1004kjs_@naver.com>
2022-02-16 21:56:23 +0900

    profile 부분 삭제

--- api/index.js
+++ api/index.js
@@ -2,13 +2,11 @@ import express from "express";
 import auth from "./controllers/auth.js";
 import univerity from "./controllers/university.js";
 import interest from "./controllers/interest.js";
-import profile from "./controllers/profile.js";
 
 const router = express.Router();
 
 router.use("/auth", auth);
 router.use("/university", univerity);
 router.use("/interest", interest);
-router.use("/profile", profile);
 
 export default router;
